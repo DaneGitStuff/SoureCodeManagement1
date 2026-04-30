20451cce3f7eebf8c50768b7268eb5fc03e6e028
Dane <Dane@DESKTOP-AGU4IRF>
2026-04-30 02:56:15 -0400

    Add project files.

---
+++ SoureCodeManagement1.cpp
@@ -0,0 +1,15 @@
+/*
+    Name: Dane Thompson
+    Class: CIS 1202 805
+    Assignment: Source Code Management 1
+    Date: April 30, 2026
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World" << endl;
+
+    return 0;
+}
